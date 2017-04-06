d0b1ff6c4713fd9bd29a7c7e560a8e9c03c4ba94
Redball45 <jamescann7654@gmail.com>
2017-04-07 00:26:50 +0100

    att fix 1

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -1,18 +1,18 @@
 import discord
 from discord.ext import commands
 
-class gw2tp:
+class Mycog:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def tpprice(self):
+    async def mycom(self):
         """This does stuff!"""
 
         #Your code will go here
         await self.bot.say("I can do stuff!")
 
 def setup(bot):
-    bot.add_cog(gw2tp(bot))
+    bot.add_cog(Mycog(bot))
