16ebbdedaabbaae8d500a235a238fa0115fd0939
Redball45 <jamescann7654@gmail.com>
2017-04-07 00:56:14 +0100

    initial tp request

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -1,18 +1,59 @@
 import discord
 from discord.ext import commands
 
+try: # check if BeautifulSoup4 is installed
+	from bs4 import BeautifulSoup
+	soupAvailable = True
+except:
+	soupAvailable = False
+import aiohttp
+...
+
+
+
 class Gw2tp:
-    """My custom cog that does stuff!"""
+    """This cog finds tp prices"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def tpprice(self):
-        """This does stuff!"""
+    async def tpbuy(self, ctx, *, tpitemid: str):
+        """This finds the current buy price of an item
+        Doesn't require any keys/scopes"""
+        user = ctx.message.author
+        try:
+            endpoint = "commerce/prices/"ctx
+            result = await self.call_api(endpoint)
+        except APIKeyError as e:
+            await self.bot.say(e)
+            return
+        except APIError as e:
+            await self.bot.say("{0.mention}, API has responded with the following error: "
+                               "`{1}`".format(user, e))
+            return
+        result = str(result).strip("['")
+        result = str(result).strip("']")
 
-        #Your code will go here
-        await self.bot.say("I can do definitelystuff!")
+await self.bot.say('ID of the guild {0} is: {1}'.format(guild, result))
+
+
+
+    async def call_api(self, endpoint):
+        apiserv = 'https://api.guildwars2.com/v2/'
+        url = apiserv + endpoint
+        async with self.session.get(url) as r:
+            results = await r.json()
+        if "error" in results:
+            raise APIError("The API is dead!")
+        if "text" in results:
+            raise APIError(results["text"])
+        return results
+
+
 
 def setup(bot):
-    bot.add_cog(Gw2tp(bot))
+	if soupAvailable:
+		bot.add_cog(Gw2tp(bot))
+	else:
+		raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
