5956b00b0bb22c175cfe58affd4af4d67dfb76c1
Redball45 <jamescann7654@gmail.com>
2017-04-07 00:06:31 +0100

    initial cog test creation

---
+++ gw2tp/gw2tp.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class gw2tp:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def tpprice(self):
+        """This does stuff!"""
+
+        #Your code will go here
+        await self.bot.say("I can do stuff!")
+
+def setup(bot):
+    bot.add_cog(Mycog(bot))
