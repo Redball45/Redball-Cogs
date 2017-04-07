018b83d3f1ec07e0b73b11679f4a61647cdab56f
Redball45 <jamescann7654@gmail.com>
2017-04-07 19:39:26 +0100

    updated some text

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -58,7 +58,7 @@ class Gw2tp:
 							   "`{1}`".format(user, e))
 			return
 		except APIError as e:
-			data = discord.Embed(description='Direct match not found, listing all - use !tpid (id)')
+			data = discord.Embed(description='Not found on the TP API, listing all - use !tpid (id)')
 			#For each item returned, add to the data table
 			shiniesendpoint = tpitemname
 			shiniesresults = await self.call_shiniesapi(shiniesendpoint)
