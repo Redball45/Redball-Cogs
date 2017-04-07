5ecff1e4f4525b4d4ac64e20a6be0fe3c9a06393
Redball45 <jamescann7654@gmail.com>
2017-04-07 14:55:29 +0100

    indentation

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -100,8 +100,8 @@ def gold_to_coins(self, money):
 	gold, remainder = divmod(money, 10000)
 	silver, copper = divmod(remainder, 100)
 	if not gold:
-	   if not silver:
-		   return "{0} copper".format(copper)
+		if not silver:
+			return "{0} copper".format(copper)
 		else:
 			return "{0} silver and {1} copper".format(silver, copper)
 	else:
