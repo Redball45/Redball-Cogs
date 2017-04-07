c0f796f8b8dad33f20037d2429633bf5c6340a8c
Redball45 <jamescann7654@gmail.com>
2017-04-07 01:16:44 +0100

    indentation fix

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -12,44 +12,44 @@ import aiohttp
 
 
 class Gw2tp:
-    """This cog finds tp prices"""
+	"""This cog finds tp prices"""
 
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.command(pass_context=True)
-    async def tpbuy(self, ctx, tpbuyid):
-        """This finds the current buy price of an item
-        Doesn't require any keys/scopes"""
-        user = ctx.message.author
-        try:
-        	commerce = 'commerce/prices/'
-            endpoint = commerce + tpbuyid
-            result = await self.call_api(endpoint)
-        except APIKeyError as e:
-            await self.bot.say(e)
-            return
-        except APIError as e:
-            await self.bot.say("{0.mention}, API has responded with the following error: "
-                               "`{1}`".format(user, e))
-            return
-        result = str(result).strip("['")
-        result = str(result).strip("']")
+	@commands.command(pass_context=True)
+	async def tpbuy(self, ctx, tpbuyid):
+		"""This finds the current buy price of an item
+		Doesn't require any keys/scopes"""
+		user = ctx.message.author
+		try:
+			commerce = 'commerce/prices/'
+			endpoint = commerce + tpbuyid
+			result = await self.call_api(endpoint)
+		except APIKeyError as e:
+			await self.bot.say(e)
+			return
+		except APIError as e:
+			await self.bot.say("{0.mention}, API has responded with the following error: "
+							   "`{1}`".format(user, e))
+			return
+		result = str(result).strip("['")
+		result = str(result).strip("']")
 
 await self.bot.say('ID of the guild {0} is: {1}'.format(guild, result))
 
 
 
-    async def call_api(self, endpoint):
-        apiserv = 'https://api.guildwars2.com/v2/'
-        url = apiserv + endpoint
-        async with self.session.get(url) as r:
-            results = await r.json()
-        if "error" in results:
-            raise APIError("The API is dead!")
-        if "text" in results:
-            raise APIError(results["text"])
-        return results
+	async def call_api(self, endpoint):
+		apiserv = 'https://api.guildwars2.com/v2/'
+		url = apiserv + endpoint
+		async with self.session.get(url) as r:
+			results = await r.json()
+		if "error" in results:
+			raise APIError("The API is dead!")
+		if "text" in results:
+			raise APIError(results["text"])
+		return results
 
 
 
