71fce1b5ad8c423046d4ee6ac6a6b3e184313de1
Redball45 <jamescann7654@gmail.com>
2017-04-08 13:55:23 +0100

    indentation

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -170,16 +170,16 @@ class Gw2tp:
 			try:
 				endpoint = 'quaggans/'
 				l_quaggans = await self.call_api(endpoint)
-			if quaggan_name == '':
-				quaggan = random.choice(l_quaggans)
-				display_given_quaggan(quaggan)
-			elif quaggan_name == list:
-				display_list_quaggans()
-			elif quaggan_name.lower() in l_quaggans:
-				display_given_quaggan(quaggan_name.lower())
-			else:
-				await self.bot.say("I couldn't find the requested quaggan. List of all available quaggans:")
-				display_list_quaggans()
+				if quaggan_name == '':
+					quaggan = random.choice(l_quaggans)
+					display_given_quaggan(quaggan)
+				elif quaggan_name == list:
+					display_list_quaggans()
+				elif quaggan_name.lower() in l_quaggans:
+					display_given_quaggan(quaggan_name.lower())
+				else:
+					await self.bot.say("I couldn't find the requested quaggan. List of all available quaggans:")
+					display_list_quaggans()
 			try:
 				await self.bot.say(embed=data)
 			except discord.HTTPException:
