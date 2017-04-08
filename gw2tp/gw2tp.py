2d215d38543370aa003b5cadeb1033b22b35d914
Redball45 <jamescann7654@gmail.com>
2017-04-08 13:44:24 +0100

    quaggans

--- gw2tp/gw2tp.py
+++ gw2tp/gw2tp.py
@@ -161,6 +161,29 @@ class Gw2tp:
 				await self.bot.say("More than 10 entries, try to refine your search")
 		except discord.HTTPException:
 			await self.bot.say("Issue embedding data into discord - EC3")
+			
+	@commands.command(pass_context=True)
+	async def quaggan(self, ctx, quaggan_name : str = ""):
+			"""This displays a quaggan"""
+			user = ctx.message.author
+			color = self.getColor(user)
+			try:
+				endpoint = "quaggans/"
+				l_quaggans = await self.call_api(endpoint)
+			if quaggan_name == "":
+				quaggan = random.choice(l_quaggans)
+				display_given_quaggan(quaggan)
+			elif quaggan_name == list:
+				display_list_quaggans()
+			elif quaggan_name.lower() in l_quaggans:
+				display_given_quaggan(quaggan_name.lower())
+			else:
+				await self.bot.say("I couldn't find the requested quaggan. List of all available quaggans:")
+				display_list_quaggans()
+			try:
+				await self.bot.say(embed=data)
+			except discord.HTTPException:
+				await self.bot.say("Issue embedding data into discord - EC3")
 
 	@commands.command(pass_context=True)
 	async def gemprice(self, ctx, numberOfGems : int = 400):
@@ -235,6 +258,16 @@ class Gw2tp:
 		except:
 			color = discord.Embed.Empty
 		return color
+
+	def display_given_quaggan(quaggan):
+		base_quaggan = r'https://static.staticwars.com/quaggans/'
+		URL_quaggan = base_quaggan + quaggan + '.jpg'
+		data = discord.Embed(title=quaggan, description='For a list of all quaggans, type !quaggan list', colour=color)
+		data.add_field(value=URL_quaggan)
+		
+	def display_list_quaggans():
+		data = discord.Embed(title=Available quaggans, colour=color)
+		data.add_field(value=', '.join(l_quaggans))
 
 def check_folders():
 	if not os.path.exists("data/guildwars2"):
