ff026e68227b5f628cee8a3c35a99569b065b331
Redball45 <jamescann7654@gmail.com>
2017-05-07 20:47:41 +0100

    name fix

--- guildwars2/guildwars2.py
+++ guildwars2/guildwars2.py
@@ -2089,7 +2089,7 @@ class Guildwars2:
 			return
 
 	@checks.is_owner()
-	@daily_notifier.command()
+	@daily_notifier.command(name="globaltoggle")
 	async def daily_global(self, on_off: bool = None):
 		"""Toggles checking for new dailies, globally.
 		Note that in order to receive notifications you to
