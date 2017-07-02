b10d0a6dc458927ebfedec8bdc43bd3c95b71916
Redball45 <jamescann7654@gmail.com>
2017-07-02 22:08:35 +0100

    finish creating rest of commands - reorganise code into its own command group "ark"

--- arkserver/arkserver.py
+++ arkserver/arkserver.py
@@ -16,35 +16,84 @@ class arkserver:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(pass_context=True)
+	@commands.group(pass_context=True)
 	@checks.mod_or_permissions(manage_webhooks=True)
+	async def ark(self, ctx):
+		"""Commands related to Ark Server Management"""
+		if ctx.invoked_subcommand is None:
+			await self.bot.send_cmd_help(ctx)
+
+	@ark.command(pass_context=True)
 	async def checkupdate(self):
-		"""Checks for ark updates"""
+		"""Checks for ark updates - does not actually start the update"""
 		output = out("arkmanager checkupdate")
 		await self.bot.say("{0}".format(output))
 
-	@commands.command(pass_context=True)
-	@checks.mod_or_permissions(manage_webhooks=True)
-	async def status(self):
+	@ark.command(pass_context=True, name="stop")
+	@checks.is_owner()
+	async def ark_stop(self):
+		"""Stops the Ark Server"""
+		output = out("arkmanager stop --warn")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="start")
+	@checks.is_owner()
+	async def ark_start(self):
+		"""Starts the Ark Server"""
+		output = out("arkmanager start")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="status")
+	async def ark_status(self):
 		"""Checks the server status"""
 		output = out("arkmanager status")
 		await self.bot.say("{0}".format(output))
 
-	@commands.command(pass_context=True)
-	@checks.mod_or_permissions(manage_webhooks=True)
-	async def arkrestart(self):
+	@ark.command(pass_context=True, name="restart")
+	async def ark_restart(self):
 		"""Restarts the ARK Server"""
-		output = out("arkmanager restart")
+		output = out("arkmanager restart --warn")
 		await self.bot.say("{0}".format(output))
 
-	@commands.command(pass_context=True)
-	@checks.mod_or_permissions(manage_webhooks=True)
-	async def arkupdate(self):
+	@ark.command(pass_context=True, name="update")
+	async def ark_update(self):
 		"""Stops the ARK Server, installs updates, then reboots"""
-		output = out("arkmanager update --update-mods")
+		output = out("arkmanager update --update-mods --backup --warn")
 		await self.bot.say("{0}".format(output))
 
-	@commands.command(pass_context=True)
+	@ark.command(pass_context=True, name="cancel")
+	async def ark_cancel(self):
+		"""Cancels a pending server shutdown"""
+		output = out("arkmanager cancelshutdown")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="save")
+	async def ark_save(self):
+		"""Saves the world state"""
+		output = out("arkmanager saveworld")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="backup")
+	async def ark_backup(self):
+		"""Creates a backup of the save and config files"""
+		output = out("arkmanager backup")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="updatenow")
+	@checks.is_owner()
+	async def ark_updatenow(self):
+		"""Updates without warning"""
+		output = out("arkmanager update --update-mods --backup")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True, name="forceupdate")
+	@checks.is_owner()
+	async def ark_forceupdate(self):
+		"""Updates without warning"""
+		output = out("arkmanager update --update-mods --backup --force")
+		await self.bot.say("{0}".format(output))
+
+	@ark.command(pass_context=True)
 	@checks.mod_or_permissions(manage_webhooks=True)
 	async def broadcast(self, ctx, *, text):
 		"""Sends a message ingame"""
