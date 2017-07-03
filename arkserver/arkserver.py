fc6cdf604bec27a0ff153f1d9cf1c78d5a94922a
Redball45 <jamescann7654@gmail.com>
2017-07-03 12:10:26 +0100

    expand automatic updater
    allowed automatic updates to be toggled and improves code to ensure an
    automatic update cannot start if the server is already restarting or
    updating

--- arkserver/arkserver.py
+++ arkserver/arkserver.py
@@ -7,10 +7,11 @@ import asyncio
 from subprocess import PIPE, run
 
 def out(command):
+	"""This function runs a shell script and collects the terminal response"""
 	result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
 	return result.stdout
 
-Updating = False
+Updating = True
 
 class arkserver:
 	"""Ark Server commands"""
@@ -38,6 +39,17 @@ class arkserver:
 		output = out("arkmanager stop")
 		await self.bot.say("{0}".format(output))
 
+	@ark.command(pass_context=True, name="toggle")
+	@checks.is_owner()
+	async def ark_toggle(self):
+		"""Toggles autoupdating - is not saved and will revert to on upon bot/cog restart"""
+		if Updating == True:
+			Updating = False
+			await self.bot.say("Automatic updating is now disabled.")
+		else:
+			Updating = True
+			await self.bot.say("Automatic server updating is now enabled.")
+
 	@ark.command(pass_context=True, name="start")
 	@checks.is_owner()
 	async def ark_start(self):
@@ -54,7 +66,8 @@ class arkserver:
 	@ark.command(pass_context=True, name="restart")
 	async def ark_restart(self, ctx, delay : int = 60):
 		"""Restarts the ARK Server - delay in seconds can be specified after restart, default 60 maximum 600"""
-		Updating = True
+		CurrentUpdating = Updating
+		Updating = False #this makes sure autoupdate does not activate while the server is already busy
 		if delay > 600:
 			delay = 600
 		await self.bot.say("Restarting in {0} seconds...".format(delay))
@@ -63,12 +76,13 @@ class arkserver:
 		await asyncio.sleep(delay)
 		output = out("arkmanager restart")
 		await self.bot.say("{0}".format(output))
-		Updating = False
+		Updating = CurrentUpdating #sets Updating back to the state it was before the command was run
 
 	@ark.command(pass_context=True, name="update")
 	async def ark_update(self, ctx, delay : int = 60):
 		"""Stops the ARK Server, installs updates, then reboots"""
-		Updating = True
+		CurrentUpdating = Updating
+		Updating = False #this makes sure autoupdate does not activate while the server is already busy
 		if delay > 600:
 			delay = 600
 		await self.bot.say("Restarting in {0} seconds...".format(delay))
@@ -77,7 +91,7 @@ class arkserver:
 		await asyncio.sleep(delay)
 		output = out("arkmanager update --update-mods --backup")
 		await self.bot.say("{0}".format(output))
-		Updating = False
+		Updating = CurrentUpdating #sets Updating back to the state it was before the command was run
 
 	@ark.command(pass_context=True, name="save")
 	async def ark_save(self):
@@ -126,19 +140,27 @@ class arkserver:
 
 	async def update_checker(self):
 		"""Checks for updates automatically every hour"""
-		while self is self.bot.get_cog("arkserver") and Updating == False:
-			output = out("arkmanager checkupdate")
-			if 'Your server is up to date!' in output:
-				await self.bot.send_message(self.bot.get_channel("331076958425186305"),"No updates found.")
-				await asyncio.sleep(3600)
-			else:
-				newoutput = out("arkmanager update --update-mods --backup --ifempty")
-				if 'players are still connected' in newoutput:
-					await self.bot.send_message(self.bot.get_channel("330795712067665923"),"An update is available but players are still connected, automatic update will not continue.".format(newoutput))
+		while self is self.bot.get_cog("arkserver"):
+			channel = self.bot.get_channel("330795712067665923")
+			adminchannel = self.bot.get_channel("331076958425186305")
+			if Updating == True:
+				output = out("arkmanager checkupdate")
+				if 'Your server is up to date!' in output:
+					await self.bot.send_message(adminchannel,"No updates found.")
 					await asyncio.sleep(3600)
 				else:
-					await self.bot.send_message(self.bot.get_channel("330795712067665923"),"{0}".format(newoutput))
-					await asyncio.sleep(3600)
+					newoutput = out("arkmanager update --update-mods --backup --ifempty")
+					if 'players are still connected' in newoutput:
+						await self.bot.send_message(channel,"An update is available but players are still connected, automatic update will not continue.".format(newoutput))
+						await asyncio.sleep(3600)
+					else:
+						await self.bot.send_message(channel,"{0}".format(newoutput))
+						await asyncio.sleep(3600)
+			else:
+				await self.bot.send_message(adminchannel,"Automatic updating is disabled, if the option is toggled on this might be because the server is already restarting or updating.")
+				await asyncio.sleep(1800)
+
+
 def setup(bot):
 	n = arkserver(bot)
 	loop = asyncio.get_event_loop()
