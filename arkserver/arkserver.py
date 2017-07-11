f89b6934f0b8488a040a0eb50d7e87f2e410f63f
Redball45 <jamescann7654@gmail.com>
2017-07-11 23:33:23 +0100

    rewrite the way status is used in runcommand
    this allows it to pass back multiple strings rather than just one

--- arkserver/arkserver.py
+++ arkserver/arkserver.py
@@ -26,7 +26,7 @@ class arkserver:
 	async def runcommand(self, command, channel, verbose):
 		"""This function runs a command in the terminal and collects the response"""
 		process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, shell=False)
-		status = "False"
+		status = ""
 		list_replacements = ["[1;32m ", "[1;31m", "[0;39m   ", "[0;39m ", "[0;39m", "8[J", "[68G[   [1;32m", "  ]", "\033"]
 		try:
 			while True:
@@ -47,18 +47,18 @@ class arkserver:
 							except discord.HTTPException as e:
 								print("Error posting to discord {0}, {1}".format(e, sani))
 					if 'Your server needs to be restarted in order to receive the latest update' in output:
-						status = 'True'
+						status = status + 'Update'
 					if 'has been updated on the Steam workshop' in output:
-						status = 'True'
+						status = status + 'ModUpdate'
 					if 'The server is now running, and should be up within 10 minutes' in output:
-						status = 'Success'
+						status = status + 'Success'
 						break
 					if 'players are still connected' in output:
-						status = 'PlayersConnected'
+						status = status + 'PlayersConnected'
 					if 'Players: 0' in output:
-						status = 'EmptyTrue'
+						status = status + 'EmptyTrue'
 					if 'online:  Yes' in output:
-						status = 'NotUpdating'
+						status = status + 'NotUpdating'
 		except:
 			await self.bot.send_message(channel,"Something went wrong... you should check the status of the server with +ark status.")
 			await self.bot.send_message(channel,"Updating and restarting options will be locked for 3 minutes for safety.")
@@ -114,7 +114,7 @@ class arkserver:
 		if minput == 'info':
 			await self.bot.say("This command can swap the map the server is running on to the desired map. Options available are 'Ragnarok' 'Island' and 'Scorched'. (e.g +ark map ragnarok)")
 			return
-		if output != 'EmptyTrue':
+		if 'EmptyTrue' in output:
 			await self.bot.say("The map cannot be swapped while players are in the server.")
 			return
 		await asyncio.sleep(5) #just to make sure previous arkmanager command has time to finish
@@ -228,7 +228,7 @@ class arkserver:
 		"""Restarts the ARK Server with a 60 second delay"""
 		channel = ctx.message.channel
 		empty = await self.runcommand("arkmanager status", channel, False)
-		if empty != 'EmptyTrue':
+		if 'EmptyTrue' in empty
 			await self.bot.say("Players are currently in the server, update anyway?")
 			answer = await self.bot.wait_for_message(timeout=30, author=user)
 			try:	
@@ -249,7 +249,7 @@ class arkserver:
 			output = await self.runcommand("arkmanager restart", channel, self.settings["Verbose"])
 			await self.bot.change_presence(game=discord.Game(name=None),status=discord.Status.online)
 			self.updating = False
-			if output == 'Success':
+			if 'Success' in output:
 				await self.bot.say("Server has been restarted.")
 			else:
 				await self.bot.say("Something went wrong :(")
@@ -260,10 +260,10 @@ class arkserver:
 		channel = ctx.message.channel
 		status = await self.runcommand("arkmanager checkupdate", channel, False)
 		modstatus = await self.runcommand("arkmanager checkmodupdate", channel, False)
-		if status == 'True' or modstatus == 'True':
+		if 'True' in status or 'True' in modstatus:
 			await self.bot.say("Updates are available.")
 			empty = await self.runcommand("arkmanager status", channel, False)
-			if empty != 'EmptyTrue':
+			if 'EmptyTrue' in empty
 				await self.bot.say("Players are currently in the server, update anyway?")
 				answer = await self.bot.wait_for_message(timeout=30, author=user)
 				try:
@@ -284,7 +284,7 @@ class arkserver:
 				output = await self.runcommand("arkmanager update --update-mods --backup", channel, self.settings["Verbose"])
 				await self.bot.change_presence(game=discord.Game(name=None),status=discord.Status.online)
 				self.updating = False
-				if output == 'Success':
+				if 'Success' in output:
 					await self.bot.say("Updates were found and installed.")
 				else:
 					await self.bot.say("Something went wrong :(")
@@ -359,12 +359,12 @@ class arkserver:
 					status = await self.runcommand("arkmanager checkupdate", adminchannel, verbose)
 					modstatus = await self.runcommand("arkmanager checkmodupdate", adminchannel, verbose)
 					await self.bot.send_message(adminchannel,"Update check completed at {0}".format(datetime.utcnow()))
-					if status == 'True' or modstatus == "True": #proceed with update if checkupdate tells us that an update is available
+					if 'Update' in status or 'ModUpdate' in modstatus: #proceed with update if checkupdate tells us that an update is available
 						await asyncio.sleep(5) #small delay to make sure previous command has cleaned up properly
 						await self.bot.change_presence(game=discord.Game(name="Updating Server"),status=discord.Status.dnd)
 						self.updating = True #this stops a manually update from being triggered by a user
 						newoutput = await self.runcommand("arkmanager update --update-mods --backup --ifempty", adminchannel, True)
-						if newoutput == 'PlayersConnected':
+						if 'PlayersConnected' in newoutput:
 							await self.bot.send_message(channel,"An update is available but players are still connected, automatic update will not continue.".format(newoutput))
 							await asyncio.sleep(15)
 							await self.bot.change_presence(game=discord.Game(name=None),status=discord.Status.online)
