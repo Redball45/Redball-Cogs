c8c020637276afae61c1a70769f197977e67a0d8
Redball45 <jamescann7654@gmail.com>
2017-10-31 16:44:58 +0000

    working version of poll

--- poll/__init__.py
+++ poll/__init__.py
@@ -1,10 +1,7 @@
-from .misc import misc
-import asyncio
+from .poll import poll
 
 
 def setup(bot):
-	n = misc(bot)
-	#loop = asyncio.get_event_loop()
-	#loop.create_task(n.gandarafullcheck())
+	n = poll(bot)
 	bot.add_cog(n)
 	bot.add_listener(n.check_poll_votes, "on_message")
